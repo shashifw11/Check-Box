85199e620243bb3d5871d780cc936747d1472634
shashifw11 <shashi_fw11_220@masai.school>
2022-09-21 19:29:00 +0530

    firstcommit

--- src/Components/MultiSelectInput.jsx
+++ src/Components/MultiSelectInput.jsx
@@ -6,28 +6,28 @@ import axios from "axios" ;
 
 export const MultiSelectInput = () => {
 
-    const data = [
-        {Country : "India" , id : 1}, 
-        {Country : "Australia" , id : 2},
-        {Country : "Srilanka" , id : 3},
-        {Country : "Nepal" , id : 4},
-        {Country : "China" , id : 5},
-        {Country : "uk" , id : 6} , 
-        {Country : "USA" , id : 7},
-        {Country : "London" , id : 8},
-        {Country : "Isrial" , id : 9},
-        {Country : "Turky" , id : 10},
-        {Country : "Thailand" , id : 11},
-        {Country : "Bangkok" , id : 12},
-        {Country : "Malasia" , id : 13},
-        {Country : "iondonasia" , id : 14},
-        {Country : "Russai" , id : 15},
-        {Country : "Britan" , id : 16},
-    ]
-    
-  const [options] = useState(data) ; 
-
   const [country , setCountry] = useState([]) ; 
+
+//   const data = [
+//     {Country : "India" , id : 1}, 
+//     {Country : "Australia" , id : 2},
+//     {Country : "Srilanka" , id : 3},
+//     {Country : "Nepal" , id : 4},
+//     {Country : "China" , id : 5},
+//     {Country : "uk" , id : 6} , 
+//     {Country : "USA" , id : 7},
+//     {Country : "London" , id : 8},
+//     {Country : "Isrial" , id : 9},
+//     {Country : "Turky" , id : 10},
+//     {Country : "Thailand" , id : 11},
+//     {Country : "Bangkok" , id : 12},
+//     {Country : "Malasia" , id : 13},
+//     {Country : "iondonasia" , id : 14},
+//     {Country : "Russai" , id : 15},
+//     {Country : "Britan" , id : 16},
+// ]
+
+// const [options] = useState(data) ; 
 
    console.log(country) ; 
 
@@ -39,7 +39,7 @@ const getData = async () =>{
     let dataCountry = await axios.get("http://localhost:3000/country").then((res)=>{
                    return res.data
         }) 
-       //   console.log(dataCountry)
+       
       setCountry(dataCountry) ; 
 }
 
